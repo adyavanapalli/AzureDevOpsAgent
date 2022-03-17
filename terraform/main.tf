1b2eeecedcec155ffa9a100dc03f51409a9d2c98
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-03-17 03:22:25 -0400

    fix: Use available image for vm.

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "azurerm_linux_virtual_machine" "virtual_machine" {
   source_image_reference {
     offer     = "UbuntuServer"
     publisher = "Canonical"
-    sku       = "21.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 }
