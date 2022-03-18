1e0917bd227a431d0cd64775a7d1271a12080cb5
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-03-18 13:15:37 -0400

    feat: Add step to install agent on VM.

---
+++ terraform/scripts/install-azuredevopsagent.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o posix
+set -o xtrace
+
+main () {
+    AGENT_DIRECTORY="$1"
+    TOKEN="$2"
+    URL="$3"
+
+    if [ ! -d "$AGENT_DIRECTORY" ]; then
+        mkdir "$AGENT_DIRECTORY"
+
+        wget --output-document - "https://vstsagentpackage.azureedge.net/agent/2.200.2/vsts-agent-linux-x64-2.200.2.tar.gz" | \
+        tar --extract --gzip --verbose --directory="$AGENT_DIRECTORY"
+
+        cd "$AGENT_DIRECTORY"
+
+        ./config.sh --token="$TOKEN" \
+                    --unattended \
+                    --url="$URL"
+
+        sudo ./svc.sh install
+        sudo ./svc.sh start
+    fi
+}
+
+main "$@"
--- terraform/scripts/install-terraform.sh
+++ terraform/scripts/install-terraform.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o posix
+set -o xtrace
+
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update
 sudo apt-get install terraform
