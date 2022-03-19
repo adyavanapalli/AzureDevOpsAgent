1e8567b335687aed6077bc26416e081d484953c9
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-03-19 19:19:57 -0400

    experiment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "linux_virtual_machine_id" {
-  value = azurerm_linux_virtual_machine.virtual_machine.id
-}
-
 output "linux_virtual_machine_name" {
   value = azurerm_linux_virtual_machine.virtual_machine.name
 }
