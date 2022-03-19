38c624854fb53a6df6a100dc245a62a471acb0bf
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-03-19 19:38:15 -0400

    experiment

--- terraform/scripts/install-azuredevopsagent.sh
+++ terraform/scripts/install-azuredevopsagent.sh
@@ -6,30 +6,25 @@ set -o pipefail
 set -o posix
 set -o xtrace
 
-# main () {
-#     AGENT_DIRECTORY="$1"
-#     TOKEN="$2"
-#     URL="$3"
+main () {
+    AGENT_DIRECTORY="$1"
+    AGENT_DOWNLOAD_URL="$2"
+    TOKEN="$3"
+    URL="$4"
 
-#     if [ ! -d "$AGENT_DIRECTORY" ]; then
-#         mkdir "$AGENT_DIRECTORY"
+    if [ ! -d "$AGENT_DIRECTORY" ]; then
+        runuser adyavanapalli --command="
+            mkdir $AGENT_DIRECTORY
 
-#         wget --output-document - "https://vstsagentpackage.azureedge.net/agent/2.200.2/vsts-agent-linux-x64-2.200.2.tar.gz" | \
-#         tar --extract --gzip --verbose --directory="$AGENT_DIRECTORY"
+            wget --output-document - $AGENT_DOWNLOAD_URL | tar --extract --gzip --verbose --directory=$AGENT_DIRECTORY
 
-#         cd "$AGENT_DIRECTORY"
+            cd $AGENT_DIRECTORY
+            ./config.sh --token=$TOKEN --unattended --url=$URL
+        "
 
-#         ./config.sh --token="$TOKEN" --unattended --url="$URL"
+        ./svc.sh install
+        ./svc.sh start
+    fi
+}
 
-#         sudo ./svc.sh install
-#         sudo ./svc.sh start
-
-#         sleep 5
-
-#         sudo ./svc.sh uninstall
-#         ./config.sh remove --unattended --token="$TOKEN"
-#     fi
-# }
-
-echo "$@"
-# main "$@"
+main "$@"
