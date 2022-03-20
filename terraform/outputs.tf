9686a2f915be88e7c4941f6d006d8654a7b1b85c
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-03-19 23:54:14 -0400

    destroy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "linux_virtual_machine_name" {
-  value = azurerm_linux_virtual_machine.virtual_machine.name
-}
+# output "linux_virtual_machine_name" {
+#   value = azurerm_linux_virtual_machine.virtual_machine.name
+# }
 
-output "resource_group_name" {
-  value = azurerm_linux_virtual_machine.virtual_machine.resource_group_name
-}
+# output "resource_group_name" {
+#   value = azurerm_linux_virtual_machine.virtual_machine.resource_group_name
+# }
